04df155d40aaef5b9191ef4fc9702a72b47ab589
soeunny11 <dlathdms1206@gmail.com>
2025-10-23 22:12:44 +0900

    storyboard제거

---
+++ LimSoEun_assignment/LimSoEun_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LimSoEun_assignment
+//
+//  Created by 임소은 on 10/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
